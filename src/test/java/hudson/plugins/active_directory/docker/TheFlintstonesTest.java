a5c950572ac792406d276be555d3ae1760bb1450
Felix Belzunce Arcos <fbelzuncearcos@cloudbees.com>
2017-11-07 19:14:06 +0100

    Update TheFlintstonesTest.java

--- src/test/java/hudson/plugins/active_directory/docker/TheFlintstonesTest.java
+++ src/test/java/hudson/plugins/active_directory/docker/TheFlintstonesTest.java
@@ -102,7 +102,7 @@ public class TheFlintstonesTest {
 
     @Test
     public void simpleLoginSuccessful() throws Exception {
-        UserDetails userDetails = j.jenkins.getSecurityRealm().loadUserByUsername("Fred");
+        UserDetails userDetails = j.jenkins.getSecurityRealm().loadUserByUsername("FredERICO");
         assertThat(userDetails.getUsername(), is("Fred"));
     }
 
